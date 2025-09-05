eadceb4fc08e8d99ce25079d5d4f4e0882baa322
Danilo <chessteacher87@gmail.com>
2025-09-05 05:52:54 +0200

    Added problem PalindromeNumber

---
+++ algorithms/problem02_PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,16 @@
+package problem02_PalindromeNumber;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+
+    private static boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        return s.contentEquals(new StringBuilder().reverse());
+    }
+
+}
