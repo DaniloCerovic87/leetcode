e8d7642e44e7a3db478585858ba9aa859d4e9247
Danilo <chessteacher87@gmail.com>
2025-09-24 21:46:26 +0200

    Added problem FirstOccurrenceInString

---
+++ algorithms/problem09_FindFirstOccurrenceInString/FirstOccurrenceInString.java
@@ -0,0 +1,31 @@
+package problem09_FindFirstOccurrenceInString;
+
+public class FirstOccurrenceInString {
+
+    public static void main(String[] args) {
+        System.out.println( strStr( "sadbutsad", "sad" ) );
+        System.out.println( strStr( "leetcode", "leeto" ) );
+    }
+
+    private static int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        if (m == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+            while (j < m && haystack.charAt( i + j ) == needle.charAt( j )) {
+                j++;
+            }
+            if (j == m) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
