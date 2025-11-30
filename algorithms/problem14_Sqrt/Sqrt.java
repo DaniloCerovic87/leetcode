eca149de68c3b403534db90e31035189041c4108
Danilo <chessteacher87@gmail.com>
2025-11-30 05:34:58 +0100

    Adding problem Sqrt

---
+++ algorithms/problem14_Sqrt/Sqrt.java
@@ -0,0 +1,33 @@
+package problem14_Sqrt;
+
+public class Sqrt {
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2));
+        System.out.println(mySqrt(8));
+        System.out.println(mySqrt(50));
+
+    }
+
+    private static int mySqrt(int x) {
+        if(x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x/2;
+        int ans = 0;
+
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if((long)mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+}
