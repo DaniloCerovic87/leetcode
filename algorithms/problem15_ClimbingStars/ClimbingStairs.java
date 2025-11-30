c3b2a7dfc3031897965a8316fa8b2b6368fc7398
Danilo <chessteacher87@gmail.com>
2025-11-30 06:35:01 +0100

    Formatting problem ClimbingStairs

--- algorithms/problem15_ClimbingStars/ClimbingStairs.java
+++ algorithms/problem15_ClimbingStars/ClimbingStairs.java
@@ -10,14 +10,14 @@ public class ClimbingStairs {
     }
 
     private static int climbStairs(int n) {
-        if(n <= 2) {
+        if (n <= 2) {
             return n;
         }
 
         int one = 1;
         int two = 2;
 
-        for(int i = 3; i <= n; i++) {
+        for (int i = 3; i <= n; i++) {
             int three = one + two;
             one = two;
             two = three;
